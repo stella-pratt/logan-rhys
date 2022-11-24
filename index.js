acb21ba62cde5974d8350949690c9e715421fea0
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2022-11-25 01:30:16 +1300

    header looking good

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log(document.documentElement.scrollTop)
-console.log(window.innerHeight)
+window.onscroll = function() {
+    let x = document.documentElement.scrollTop/window.innerHeight
+    let h = 0.8
+    let a = -(25/16)
+    let k = 1
+    let transparency = a*(x-h)**2 + k
+    if(x-h>0) {
+        transparency = 1
+    }
+    let shader = document.querySelector(".shade")
+    shader.style.backgroundColor = "rgba(0,0,0," + transparency + ")"
+
+
+    let text = document.querySelector(".header_text")
+
+    text.style.bottom = (window.innerHeight / 100 * 3) + 0.0015 * document.documentElement.scrollTop**2 + "px"
+
+
+
+
+
+
+}
